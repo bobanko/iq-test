44731bf34241f6b417cec14a16c08c46111f6123
Volodymyr Myshko <v.myshko.synergetica@gmail.com>
2024-08-03 23:43:51 +0300

    refac: random helpers extract, seeded random impl WIP

--- helpers.js
+++ helpers.js
@@ -1,26 +1,10 @@
-export function pickRandom(array) {
-  const randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
-}
-
-export function spliceRandom(array) {
-  const randomIndex = Math.floor(Math.random() * array.length);
-
-  return array.splice(randomIndex, 1)[0];
-}
-
-export function shuffle(array) {
-  const result = [...array];
-
-  result.sort(() => 0.5 - Math.random());
-
-  return result;
-}
-
-export function fromRange(min, max) {
-  const randomIndex = Math.floor(min + Math.random() * (max - min + 1));
-  return randomIndex;
-}
+export {
+  pickRandom,
+  spliceRandom,
+  shuffle,
+  fromRange,
+  setRandomSeed,
+} from "./random.helpers.js";
 
 export function preventSvgCache() {
   // todo(vmyshko): debug: prevent svg cache
---
+++ random.helpers.js
@@ -0,0 +1,36 @@
+import { seededRandom } from "./seeded-random.js";
+
+export function setRandomSeed(seed = 1) {
+  window.random = seededRandom(seed);
+}
+
+function random() {
+  return window.random();
+}
+
+export function pickRandom(array) {
+  const randomIndex = Math.floor(random() * array.length);
+  return array[randomIndex];
+}
+
+export function spliceRandom(array) {
+  const randomIndex = Math.floor(random() * array.length);
+
+  return array.splice(randomIndex, 1)[0];
+}
+
+export function shuffle(array) {
+  const result = [...array];
+
+  result.sort(() => 0.5 - random());
+
+  return result;
+}
+
+export function fromRange(min, max) {
+  const randomIndex = Math.floor(min + random() * (max - min + 1));
+  return randomIndex;
+}
+
+//basic init
+setRandomSeed();
