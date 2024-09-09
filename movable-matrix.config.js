fbfe8174c998dc5696bfcaff2fbacf60acb22f75
Volodymyr Myshko <v.myshko.synergetica@gmail.com>
2024-09-09 04:54:43 +0300

    feat: include some new tests for movable-matrix

--- movable-matrix.config.js
+++ movable-matrix.config.js
@@ -3,7 +3,7 @@ import { renderMovableQuestion } from "./matrix.renderer.js";
 
 const _movableConfigs = {
   //  desc here
-  easy1: {
+  color1ruleMix: {
     colorCount: 1, // how many cells will be painted
     ruleSet: 2, // [TRDL movement, diagonal, both, ...]
 
@@ -12,11 +12,23 @@ const _movableConfigs = {
     shiftColorsBetweenRows: false,
     noOverlap: false, // [2 and more] figs can overlap each other - have same deg
   },
-  medium2: {
+  color1ruleOrt: {
+    colorCount: 1, // how many cells will be painted
+    ruleSet: 0,
+  },
+  color2ruleOrt: {
+    colorCount: 2, // how many cells will be painted
+    ruleSet: 0,
+  },
+  color2ruleDiag: {
     colorCount: 2, // how many cells will be painted
     ruleSet: 1,
   },
-  hard3: {
+  color2ruleMix: {
+    colorCount: 2, // how many cells will be painted
+    ruleSet: 2,
+  },
+  color3ruleOrt: {
     colorCount: 3, // how many cells will be painted
     ruleSet: 0,
   },
