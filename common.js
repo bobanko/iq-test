463343f12cc7b3cde0414ac01f1d75d46bf9fdce
Volodymyr Myshko <v.myshko.synergetica@gmail.com>
2024-09-11 22:13:39 +0300

    chore: clamp impl

--- common.js
+++ common.js
@@ -4,3 +4,14 @@ export const getUid = (() => {
 
   return () => `${_id++}`;
 })();
+
+/**
+ *
+ * @param {number} num number to clamp
+ * @param {number} min min allowed value
+ * @param {number} max max allowed value
+ * @returns clamped value
+ */
+export function clamp(num, min, max) {
+  return Math.min(Math.max(num, min), max);
+}
