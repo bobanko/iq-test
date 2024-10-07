4dd0cd65157d9699b293ad3ff6463b1e030ca315
Volodymyr Myshko <v.myshko.synergetica@gmail.com>
2024-10-07 23:41:57 +0300

    refac: scaleViewBox allow to scale x,y separately

--- common.js
+++ common.js
@@ -19,17 +19,18 @@ export function clamp(num, min, max) {
 /**
  *
  * @param {string} viewBox like '0 0 100 100'
- * @param {number} scale 0.5 is 50%, 1.5 is 150%
+ * @param {number} scaleX 0.5 is 50%, 1.5 is 150%
+ * @param {number} scaleY 0.5 is 50%, 1.5 is 150%, defaults to scaleX
  * @returns scaled viewBox
  */
-export function scaleViewBox(viewBox, scale) {
+export function scaleViewBox(viewBox, scaleX, scaleY = scaleX) {
   const [x, y, width, height] = viewBox.split(" ").map((value) => +value);
 
   return [
-    x + (width - width / scale) / 2,
-    y + (height - height / scale) / 2,
-    width / scale,
-    height / scale,
+    x + (width - width / scaleX) / 2,
+    y + (height - height / scaleY) / 2,
+    width / scaleX,
+    height / scaleY,
   ]
     .map((value) => value.toFixed(2))
     .join(" ");
