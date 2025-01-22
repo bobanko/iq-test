d09e1e2377e556e914b9f89b07ccecd648b919bf
Volodymyr Myshko <v.myshko.synergetica@gmail.com>
2025-01-22 22:05:06 +0200

    variant: add crop figure alt variant (no code optimizations)

--- crop-figures.config.js
+++ crop-figures.config.js
@@ -1,5 +1,8 @@
 import { defaultColors } from "./common.config.js";
-import { generateCropFigurePatternsQuestion } from "./crop-figures.generator.js";
+import {
+  generateCropFigurePatternsQuestion,
+  generateCropFigurePatternsQuestionAlt,
+} from "./crop-figures.generator.js";
 import { renderFigurePatternsQuestion } from "./figure-patterns.renderer.js";
 
 const defaultViewBox = "0 0 100 100";
@@ -7,7 +10,7 @@ const defaultViewBox = "0 0 100 100";
 export const cropFiguresConfigs = {
   cropFigures1: {
     patternsInCol: 3,
-    patternsInCol: 3,
+    patternsInRow: 3,
     viewBox: defaultViewBox,
     noRotationAnimation: true,
 
@@ -30,6 +33,34 @@ export const cropFiguresConfigs = {
     ],
 
     generator: generateCropFigurePatternsQuestion,
+    renderer: renderFigurePatternsQuestion,
+  },
+
+  cropFigures2: {
+    patternsInCol: 2,
+    patternsInRow: 3,
+    viewBox: defaultViewBox,
+    noRotationAnimation: true,
+
+    maxAnswerCount: 6,
+
+    figureLink: "./images/xor-halves.svg",
+
+    figures: ["half-1", "half-2", "separator"],
+    figureColors: [...defaultColors],
+    cropFigures: [
+      "cutout-1",
+      "cutout-2",
+      "cutout-3",
+      "cutout-4",
+      "cutout-5",
+      "cutout-6",
+      "cutout-7",
+      "cutout-8",
+      "cutout-9",
+    ],
+
+    generator: generateCropFigurePatternsQuestionAlt,
     renderer: renderFigurePatternsQuestion,
   },
 };
--- crop-figures.generator.js
+++ crop-figures.generator.js
@@ -179,3 +179,196 @@ export function generateCropFigurePatternsQuestion({
     //
   };
 }
+
+// **************
+// todo(vmyshko): funcs below copied from above, and modified, similar logic should be extracted
+
+function* cropFiguresGeneratorAlt({ random, config }) {
+  const { patternsInCol = 3, patternsInRow = 3 } = config;
+
+  const { figures, figureColors, cropFigures } = config;
+
+  const cropFiguresShuffled = random.shuffle([...cropFigures]);
+
+  for (let rowIndex = 0; rowIndex < patternsInCol; rowIndex++) {
+    const [color1, color2, color3] = random.popRangeFrom([...figureColors], 3);
+
+    const bgRotation = random.sample([0, 90]);
+
+    const cropFigureRandom =
+      cropFiguresShuffled[rowIndex % cropFiguresShuffled.length];
+
+    // basic figure
+    yield {
+      figureParts: [
+        {
+          figures: [figures[0]],
+          color: color1,
+          rotation: bgRotation,
+        },
+        {
+          figures: [figures[1]],
+          color: color2,
+          rotation: bgRotation,
+        },
+        {
+          figures: [figures[2]],
+          rotation: bgRotation,
+        },
+      ],
+    };
+
+    // figure to crop
+    yield {
+      figureParts: [
+        //prev figs from col-1
+        {
+          figures: [figures[0]],
+          color: color1,
+          rotation: bgRotation,
+        },
+        {
+          figures: [figures[1]],
+          color: color2,
+          rotation: bgRotation,
+        },
+        {
+          figures: [figures[2]],
+          rotation: bgRotation,
+        },
+        //...plus extra fig
+
+        {
+          figures: [cropFigureRandom],
+          color: color3,
+          strokeWidth: 2,
+          stroke: colors.dark,
+          rotation: 0,
+        },
+      ],
+    };
+    // 3rd col - just extra crop fig
+    yield {
+      figureParts: [
+        {
+          figures: [cropFigureRandom],
+          color: color3,
+          strokeWidth: 2,
+          stroke: colors.dark,
+          rotation: 0,
+        },
+      ],
+    };
+  } //row
+}
+
+function generateAnswerAlt({ random, config, correctAnswer }) {
+  const { figureParts, colorGroups, rotationGroups } = config;
+
+  const { patternsInCol = 3, patternsInRow = 3 } = config;
+
+  const { figures, figureColors, cropFigures } = config;
+
+  // todo(vmyshko): some figs looks same after 90 rot (circle and square) so skip rotation
+  const bgRotation = random.sample([0, 90]);
+  // todo(vmyshko): get rid of this copy-pasta
+
+  const [color1, color2] = random.popRangeFrom([...figureColors], 2);
+
+  return {
+    isCorrect: false,
+    id: getUid(),
+    figureParts: random.sample([
+      // full bg
+      [
+        {
+          figures: [figures[0]],
+          color: color1,
+          rotation: bgRotation,
+        },
+        {
+          figures: [figures[1]],
+          color: color2,
+          rotation: bgRotation,
+        },
+        {
+          figures: [figures[2]],
+          rotation: bgRotation,
+        },
+      ],
+      //OR crop fig
+      [
+        {
+          figures: [random.sample(cropFigures)],
+          //figures: [random.sample(figures)],
+          color: random.sample(figureColors),
+          strokeWidth: 2,
+          stroke: colors.dark,
+          rotation: 0, //cause some crops has no visual diff when rotated
+        },
+      ],
+    ]),
+  };
+}
+
+export function generateCropFigurePatternsQuestionAlt({
+  config,
+  seed,
+  questionIndex,
+}) {
+  //
+  const {
+    patternsInRow = 3,
+    maxAnswerCount = 6, //over 8 will not fit
+    figureCount, // single pattern figure count [2..n]
+  } = config;
+
+  const random = new SeededRandom(seed + questionIndex);
+
+  const patterns = [
+    ...cropFiguresGeneratorAlt({
+      random,
+      config,
+    }),
+  ];
+
+  //last block
+  const [correctAnswer] = patterns.splice(-1, 1, null);
+  correctAnswer.isCorrect = true;
+  correctAnswer.id = getUid();
+
+  // *******
+  // ANSWERS
+  // *******
+
+  const answers = generateUniqueValues({
+    existingValues: [correctAnswer],
+    maxValuesCount: maxAnswerCount - 1,
+    generateFn: () => {
+      return generateAnswerAlt({
+        correctAnswer,
+        random,
+        config,
+      });
+    },
+    getValueHashFn: ({ figureParts = [] }) =>
+      figureParts
+        .map(
+          // todo(vmyshko): why defaults are not set yet? re-check!
+          ({ figures, rotation = 0, color = "", strokeWidth = 0 }) =>
+            `${figures.toString()};${color};${rotation};${strokeWidth}`
+        )
+        .toString(),
+  });
+
+  // todo(vmyshko): review, do those all are used?
+  return {
+    seed,
+    patternsInRow,
+    //
+    patterns,
+    answers,
+    correctAnswer,
+    //
+  };
+}
