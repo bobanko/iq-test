578d0eb62b7f06cfb3ef8072d60cebdd96c03aea
bobanko <vladimir.mishko@gmail.com>
2025-01-28 22:31:32 +0200

    refac: declare generator and renderer per config block + iq-n prefix

--- boolean-matrix.config.js
+++ boolean-matrix.config.js
@@ -4,47 +4,58 @@ import {
   ruleSets,
 } from "./boolean-matrix.generator.js";
 
-const _booleanMatrixConfigs = {
-  add: {
+export const booleanMatrixConfigs = {
+  iq7_vseq_colorDiff: {
+    ruleSet: ruleSets.colorDiffRowPatterns,
+    patternsInCol: 3,
+    mtxSize: 3,
+
+    // todo(vmyshko): impl v sequence?
+    generator: generateBooleanMatrixQuestion,
+    renderer: renderMatrixQuestion,
+  },
+
+  iq8_sub: {
+    ruleSet: ruleSets.subRowPatterns,
+    patternsInCol: 3,
+    mtxSize: 3,
+
+    generator: generateBooleanMatrixQuestion,
+    renderer: renderMatrixQuestion,
+  },
+
+  iq9_add: {
     ruleSet: ruleSets.addRowPatterns,
     patternsInCol: 3,
     mtxSize: 3,
     // maxAnswerCount: 20,
+
+    generator: generateBooleanMatrixQuestion,
+    renderer: renderMatrixQuestion,
   },
-  sub: {
-    ruleSet: ruleSets.subRowPatterns,
-    patternsInCol: 3,
-    mtxSize: 3,
-  },
-  colorDiff: {
-    ruleSet: ruleSets.colorDiffRowPatterns,
-    patternsInCol: 3,
-    mtxSize: 3,
-  },
-  addSubColor: {
+
+  iq10_addSubColor: {
     ruleSet: ruleSets.addAndSubRowPatterns,
     patternsInCol: 3,
     mtxSize: 3,
+
+    generator: generateBooleanMatrixQuestion,
+    renderer: renderMatrixQuestion,
   },
   xor: {
     ruleSet: ruleSets.xorRowPatterns,
     patternsInCol: 3,
     mtxSize: 3,
+
+    generator: generateBooleanMatrixQuestion,
+    renderer: renderMatrixQuestion,
   },
   sumAll3: {
     ruleSet: ruleSets.sumAll3RowPatterns,
     patternsInCol: 3,
     mtxSize: 3,
+
+    generator: generateBooleanMatrixQuestion,
+    renderer: renderMatrixQuestion,
   },
 };
-
-export const booleanMatrixConfigs = Object.fromEntries(
-  Object.entries(_booleanMatrixConfigs).map((entry) => {
-    const [_key, value] = entry;
-
-    value.generator = generateBooleanMatrixQuestion;
-    value.renderer = renderMatrixQuestion;
-
-    return entry;
-  })
-);
