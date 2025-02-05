a6fea7a90facca46edbd8eadd07dd5e33bbc6225
bobanko <vladimir.mishko@gmail.com>
2025-02-05 07:53:31 +0200

    chore: number-progressions keep 4 for quiz

--- number-progressions.config.js
+++ number-progressions.config.js
@@ -4,90 +4,118 @@ import {
 } from "./number-progressions.generator.js";
 import { renderValuesQuestion } from "./value.renderer.js";
 
-const _numberProgressionConfigs = {
+export const numberProgressionConfigs = {
   equalSumPerRowCol: {
+    // skip: true,
     progressionType: progressionTypes.equalSumPerRowCol,
     patternsInRow: 3,
     patternsInCol: 3,
     maxAnswerCount: 6,
     // todo(vmyshko): sum can be random
     colRowSum: 8,
+
+    generator: generateNumberProgressionQuestion,
+    renderer: renderValuesQuestion,
   },
   //  desc here
   iq15_numToAbc: {
+    skip: true,
     progressionType: progressionTypes.numToAbc,
-    patternsInCol: 5, // todo(vmyshko): rename to row count or smth
+    patternsInCol: 2, // todo(vmyshko): rename to row count or smth
     maxAnswerCount: 6,
+
+    generator: generateNumberProgressionQuestion,
+    renderer: renderValuesQuestion,
   },
   iq16_addToAbc: {
     // todo(vmyshko): sort by asc
     progressionType: progressionTypes.addToAbc,
     patternsInCol: 3, // limits by maxAnswerCount as well
     maxAnswerCount: 6,
+
+    generator: generateNumberProgressionQuestion,
+    renderer: renderValuesQuestion,
   },
   iq17_addProgression: {
+    skip: true,
     // todo(vmyshko): seems orig 135 is not possible
     progressionType: progressionTypes.addProgression,
     patternsInCol: 3,
     maxAnswerCount: 6,
     maxRange: 20,
+
+    generator: generateNumberProgressionQuestion,
+    renderer: renderValuesQuestion,
   },
   iq18_subProgression: {
+    skip: true,
     // todo(vmyshko): impossible orig 741
     progressionType: progressionTypes.subProgression,
     patternsInCol: 3,
     maxAnswerCount: 6,
     maxRange: 20,
+
+    generator: generateNumberProgressionQuestion,
+    renderer: renderValuesQuestion,
   },
   subIncrementAll: {
+    // skip: true,
     progressionType: progressionTypes.subIncrementAll,
     patternsInRow: 3,
     patternsInCol: 3,
     maxAnswerCount: 6,
     maxRange: 10,
+
+    generator: generateNumberProgressionQuestion,
+    renderer: renderValuesQuestion,
   },
   subPerRow: {
+    skip: true,
     progressionType: progressionTypes.subPerRow,
     patternsInRow: 3,
     patternsInCol: 3,
     maxAnswerCount: 6,
     maxRange: 30,
+
+    generator: generateNumberProgressionQuestion,
+    renderer: renderValuesQuestion,
   },
 
   // todo(vmyshko): not sure about config.maxRange for those... should it be configurable at all?
   // same for row/col
   addThenDiv: {
+    skip: true,
     progressionType: progressionTypes.addThenDiv,
     patternsInRow: 3,
     patternsInCol: 3,
     maxAnswerCount: 6,
     maxRange: 20,
+
+    generator: generateNumberProgressionQuestion,
+    renderer: renderValuesQuestion,
   },
 
   mulThenSubProgression: {
+    skip: true,
     progressionType: progressionTypes.mulThenSubProgression,
     patternsInRow: 3,
     patternsInCol: 3,
     maxAnswerCount: 6,
     maxRange: 10,
+
+    generator: generateNumberProgressionQuestion,
+    renderer: renderValuesQuestion,
   },
 
   subThenAdd: {
+    // skip: true,
     progressionType: progressionTypes.subThenAdd,
     patternsInRow: 3,
     patternsInCol: 3,
     maxAnswerCount: 6,
     maxRange: 15, //maxDiff
+
+    generator: generateNumberProgressionQuestion,
+    renderer: renderValuesQuestion,
   },
 };
-
-export const numberProgressionConfigs = Object.fromEntries(
-  Object.entries(_numberProgressionConfigs).map((entry) => {
-    const [_key, value] = entry;
-
-    value.generator = generateNumberProgressionQuestion;
-    value.renderer = renderValuesQuestion;
-
-    return entry;
-  })
-);
