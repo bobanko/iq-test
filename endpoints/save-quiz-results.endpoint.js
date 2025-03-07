21a9d73b6cb7d62a8fad63f1d71d3825a2425026
bobanko <vladimir.mishko@gmail.com>
2025-03-07 03:56:36 +0200

    refac: saveQuizResults seed fix, data ordering

--- endpoints/save-quiz-results.endpoint.js
+++ endpoints/save-quiz-results.endpoint.js
@@ -1,23 +1,21 @@
 import { collection, doc, serverTimestamp, setDoc } from "./firebase.js";
 import { db, getCurrentUser } from "./firebase.init.js";
 
-export async function saveQuizResults({ quizResults, quizData = {}, stats }) {
+export async function saveQuizResults({ quizResults, seed, stats }) {
   // set
-
-  const { answers, seed = 123 } = quizData;
 
   const user = await getCurrentUser();
 
   const quizResultRef = doc(collection(db, "quiz-results"));
 
   await setDoc(quizResultRef, {
-    userId: user.uid,
+    _userId: user.uid,
     datePassed: serverTimestamp(),
 
     seed,
     stats, //answered,correct,wrong iq etc...
     // to keep it at the end in firebase doc
-    quizResults: quizResults,
+    zAnswers: quizResults,
   });
 
   console.log("quizResultRef written with ID: ", quizResultRef.id);
