910737471cdbb536a53dc28afe05fa99f2d0fb7a
bobanko <vladimir.mishko@gmail.com>
2025-03-10 06:28:15 +0200

    feat: easter-egg requested by anis

--- renderers/common.renderer.js
+++ renderers/common.renderer.js
@@ -4,5 +4,35 @@ export function createQuestionMark({ classList = [] } = {}) {
   //additional classes
   classList.forEach((_class) => $patternQuestionMark.classList.add(_class));
 
+  //easter-egg
+  let countdown = 10;
+  $patternQuestionMark.addEventListener("click", questionMarkClick);
+
+  function questionMarkClick() {
+    countdown--;
+    $patternQuestionMark.animate(
+      [
+        // keyframes
+        { transform: "rotate(30deg)" },
+        { transform: "rotate(0deg)" },
+      ],
+      {
+        // timing options
+        duration: 200,
+        easing: "ease-in-out",
+        iterations: 1,
+      }
+    );
+
+    const symbols = ["🖕", "🖕🏻", "🖕🏼", "🖕🏽", "🖕🏾", "🖕🏿"];
+
+    if (countdown <= 0) {
+      $patternQuestionMark.setAttribute(
+        "symbol",
+        symbols[(symbols.length - countdown) % symbols.length]
+      );
+    }
+  }
+
   return $patternQuestionMark;
 }
