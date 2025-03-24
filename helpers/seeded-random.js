35c68de809fd341a36d51d87661de9ed359cbd4f
bobanko <vladimir.mishko@gmail.com>
2025-03-24 16:22:32 +0200

    refac: normalize seed to 0..2**32 range

--- helpers/seeded-random.js
+++ helpers/seeded-random.js
@@ -1,28 +1,39 @@
+const modulus = 2 ** 32; // 2^32 modulus
+
+export function getNormalizedSeed(rawSeed01 = Math.random()) {
+  if (rawSeed01 > 0 && rawSeed01 < 1) {
+    return Math.floor(rawSeed01 * modulus);
+  } else {
+    throw Error("raw seed should be in bounds [0,1]", rawSeed01);
+  }
+}
+
 export function getSeededRandom(rawSeed) {
   // Park-Miller LCG algorithm
-  const m = 2 ** 32; // 2^32 modulus
   const a = 1664525; // multiplier
   const c = 1013904223; // increment
 
-  let stateSeed = rawSeed || Math.floor(Math.random() * m);
+  if (isNaN(rawSeed)) {
+    console.warn(
+      `seed should be number, 0..${modulus} (2^32); provided seed: ${rawSeed}`
+    );
+  }
+
+  let stateSeed = isNaN(rawSeed) ? getNormalizedSeed() : rawSeed;
+  // for backward compatibility with Math.random() values
+  if (rawSeed > 0 && rawSeed < 1) {
+    // if math.random provided as seed
+    stateSeed = getNormalizedSeed(rawSeed);
+  }
 
   const randomFn = () => {
-    stateSeed = (a * stateSeed + c) % m;
-    return stateSeed / m;
+    stateSeed = (a * stateSeed + c) % modulus;
+    return stateSeed / modulus;
   };
 
   // todo(vmyshko): for seed 0..1 do progrev
   // todo(vmyshko): to fig rng first pseudo random results
-  randomFn(), randomFn(), randomFn(), randomFn(), randomFn();
+  // randomFn(), randomFn(), randomFn(), randomFn(), randomFn();
 
   return randomFn;
 }
-
-// // Example usage:
-// const seed = 123; // Seed for reproducibility
-// const rng = getSeededRandom(seed);
-
-// // Generate random numbers
-// console.log(rng()); // First random number
-// console.log(rng()); // Second random number
-// console.log(rng()); //
