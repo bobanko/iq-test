4d4a37a478e8845fb3032a7884c873b66d8ca2cf
bobanko <vladimir.mishko@gmail.com>
2025-04-12 20:35:10 +0300

    fix: fractional iq result

--- calc-iq.js
+++ calc-iq.js
@@ -1,8 +1,17 @@
+/**
+ *
+ * @param {Object} stats
+ * @param stats.isCorrect correctly answered count
+ * @param stats.total question count
+ * @returns
+ */
 export function calcStaticIqByStats(stats) {
   const minIq = 60;
   const maxIq = 140;
 
   const { isCorrect, total } = stats;
 
-  return minIq + (isCorrect / total) * (maxIq - minIq);
+  const resultIq = minIq + (isCorrect / total) * (maxIq - minIq);
+
+  return Math.floor(resultIq);
 }
