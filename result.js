560b3ab589580f61783f969399c9d80954f18fc4
bobanko <mishko.vladimir@gmail.com>
2026-04-12 08:41:30 +0300

    feat: add reptilian brain

--- result.js
+++ result.js
@@ -207,12 +207,17 @@ async function displayResult({ userResult, allResults }) {
 
   const { isAnswered, isCorrect, timeSpent, total } = resultsStats;
 
+  // todo(vmyshko): make based on modifiers?
   if (isAnswered === 0) {
     $brainImg.src = "./images/brains/brain-dummy.png";
   } else if (isCorrect < 3) {
     $brainImg.src = "./images/brains/brain-dead.png";
   } else if (isCorrect <= 7) {
     $brainImg.src = "./images/brains/brain-smoker.png";
+  }
+
+  if (isCorrect > 10 && isCorrect < 25 && timeSpent < 5 * 60 * 1000) {
+    $brainImg.src = "./images/brains/brain-reptilian.png";
   }
 
   $brainImg.parentElement.classList.remove("blurred");
